10829c1ee2b5e8e6f16a026bf62d69e497706d49
Ranajoy66 <indranil.ranajoy66@gmail.com>
2024-08-21 02:30:00 +0530

    Frontend and Backend

---
+++ server/models/Admin.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const AdminSchema = new mongoose.Schema({
+    email: String,
+    pass: String
+})
+
+const AdminModel = mongoose.model("admins", AdminSchema)
+module.exports = AdminModel
--- src/components/Price.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Price () {
-    return (
-        <h1>PRICE LIST</h1>
-    );
-}
-
-export default Price;
